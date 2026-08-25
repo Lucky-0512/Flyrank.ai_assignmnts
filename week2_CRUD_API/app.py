400d1dc5406883ca1dc44c787fcc1e7cf8c4536c
Lucky-0512 <cultdraw999@gmail.com>
2026-08-26 00:43:55 +0530

    Stage 2: read endpoints with 404

--- week2_CRUD_API/app.py
+++ week2_CRUD_API/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -10,6 +11,31 @@ def greet():
 def itemslo():
     return { "name": "Task API", "version": "1.0", "endpoints": ["/tasks"] }    
 
+class entry(BaseModel):
+    id:int
+    title:str
+    done: bool
+
+
 @app.get("/health")
 def health():
     return {'status':'OK'}
+
+## stage 2
+memory:list[dict] = [
+    {'id':1,'title':"finish my assigemtn , i wanna fly to bosnia ;).","done":False},
+    {'id':2,'title':"do chores .","done":False},
+    {'id':3,'title':"walk out to the park","done":True}
+]
+
+@app.get("/tasks")
+def get_tasks():
+    return memory
+
+@app.get("/tasks/{id}")
+def get_one(id:int):
+    if id > len(memory):
+        return { "error": "Task 99 not found", 'status':404 }
+    return {"data":memory[id-1],'status':200}
+
+
